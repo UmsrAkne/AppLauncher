8118af093e09ad52d4ae70c0e2e2ac044ec56c85
Akn <sibata486@gmail.com>
2024-12-23 19:20:02 +0900

    add feat / コンテキストメニューから任意のアプリを再起動する機能を実装。

--- AppLauncher/Models/ApplicationInfo.cs
+++ AppLauncher/Models/ApplicationInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Prism.Mvvm;
 
@@ -20,5 +21,30 @@ namespace AppLauncher.Models
         public bool IsRunning { get => isRunning; set => SetProperty(ref isRunning, value); }
 
         public Process Process { get => process; set => SetProperty(ref process, value); }
+
+        public AsyncDelegateCommand RestartAppAsyncCommand => new AsyncDelegateCommand(async () =>
+        {
+            try
+            {
+                Process.Kill(); // プロセスを強制終了
+                await Process.WaitForExitAsync(); // 終了を非同期で待機
+                Process.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"プロセスの終了中にエラーが発生しました: {ex.Message}");
+                return;
+            }
+
+            try
+            {
+                Debug.WriteLine($"新しいアプリケーション {FullPath} を起動します...");
+                Process = Process.Start(FullPath);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"新しいアプリケーションの起動に失敗しました: {ex.Message}");
+            }
+        });
     }
 }

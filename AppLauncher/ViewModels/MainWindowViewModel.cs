f0a37c5f5af4b430735f7716cd6ba84687804b38
Akn <sibata486@gmail.com>
2024-12-23 18:11:13 +0900

    add feat / ダブルクリックで選択した実行ファイルを起動する機能を実装した。

--- AppLauncher/ViewModels/MainWindowViewModel.cs
+++ AppLauncher/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using AppLauncher.Models;
 using Prism.Mvvm;
 
@@ -17,13 +20,59 @@ namespace AppLauncher.ViewModels
 
         public string Title { get => title; set => SetProperty(ref title, value); }
 
-        public ObservableCollection<ApplicationInfo> ApplicationInfos { get; private set; } = new ();
+        public ApplicationListViewModel ApplicationListViewModel { get; set; } = new ();
+
+        /// <summary>
+        /// ApplicationListViewModel の中で、選択中のアイテムを起動します。<br/>
+        /// 選択中のアイテムが無い場合や、無効なパスが入っている場合は処理をせず動作を終了します。
+        /// </summary>
+        public AsyncDelegateCommand ExecuteAppCommand => new AsyncDelegateCommand(async () =>
+        {
+            var appInfo = ApplicationListViewModel.SelectedItem;
+
+            if (appInfo == null || !File.Exists(appInfo.FullPath))
+            {
+                return;
+            }
+
+            var filePath = appInfo.FullPath;
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    await Task.Run(() =>
+                    {
+                        appInfo.Process = Process.Start(new ProcessStartInfo
+                        {
+                            FileName = filePath,
+                            UseShellExecute = true,
+                        });
+
+                        appInfo.IsRunning = true;
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                Debug.WriteLine($"The file '{filePath}' does not exist.");
+            }
+        });
 
         [Conditional("DEBUG")]
         private void SetDummies()
         {
-            ApplicationInfos = new ObservableCollection<ApplicationInfo>()
+            ApplicationListViewModel.ApplicationInfos = new ObservableCollection<ApplicationInfo>()
             {
+                new ApplicationInfo()
+                {
+                    DisplayName = "Application1",
+                    FullPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\test\test.exe",
+                },
                 new ApplicationInfo() { DisplayName = "testName1", FullPath = @"C:\test\test1\test2\test3\text4", },
                 new ApplicationInfo() { DisplayName = "testName2", FullPath = @"C:\test\test1\test2\test3\text4", },
                 new ApplicationInfo() { DisplayName = "testName3", FullPath = @"C:\test\test1\test2\test3\text4", },

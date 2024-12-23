840e746f5302cb43d28644b59fef5f236f832ec0
Akn <sibata486@gmail.com>
2024-12-23 16:07:05 +0900

    init /

---
+++ AppLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace AppLauncher.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "App Launcher";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}

de0c8317658f09e4d1429290f59f84707cdf0186
Akn <sibata486@gmail.com>
2024-12-24 22:44:17 +0900

    add feat / アプリをリストに追加するコマンドを実装した。

--- AppLauncher/ViewModels/MainWindowViewModel.cs
+++ AppLauncher/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using AppLauncher.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace AppLauncher.ViewModels
@@ -10,6 +11,7 @@ namespace AppLauncher.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         private string title = "App Launcher";
+        private ApplicationInfo inputApplicationInfo = new ();
 
         public MainWindowViewModel()
         {
@@ -19,6 +21,24 @@ namespace AppLauncher.ViewModels
         public string Title { get => title; set => SetProperty(ref title, value); }
 
         public ApplicationListViewModel ApplicationListViewModel { get; set; } = new ();
+
+        public ApplicationInfo InputApplicationInfo
+        {
+            get => inputApplicationInfo;
+            set => SetProperty(ref inputApplicationInfo, value);
+        }
+
+        public DelegateCommand RegisterAppCommand => new DelegateCommand(() =>
+        {
+            var info = InputApplicationInfo;
+            if (string.IsNullOrWhiteSpace(info.DisplayName) || string.IsNullOrWhiteSpace(info.FullPath))
+            {
+                return;
+            }
+
+            ApplicationListViewModel.ApplicationInfos.Add(info);
+            InputApplicationInfo = new ApplicationInfo();
+        });
 
         [Conditional("DEBUG")]
         private void SetDummies()

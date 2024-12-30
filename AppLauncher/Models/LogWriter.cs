e039f6e0cdaf02020cbec600f73e93da77d4872b
Akn <sibata486@gmail.com>
2024-12-30 14:30:06 +0900

    refactoring /

--- AppLauncher/Models/LogWriter.cs
+++ AppLauncher/Models/LogWriter.cs
@@ -3,32 +3,31 @@ namespace AppLauncher.Models
     using System;
     using System.IO;
 
-    public class LogWriter
+    public static class LogWriter
     {
         private static readonly string LogFilePath = "log.txt";
 
         public static void AppendAppStartLog(string fileName)
         {
-            AppendTextToFile(LogFilePath, $"{DateTime.Now} {fileName} を起動しました。");
+            AppendTextToFile($"{DateTime.Now} {fileName} を起動しました。");
         }
 
         public static void ErrorLog(string message)
         {
-            AppendTextToFile(LogFilePath, $"{DateTime.Now} {message}");
+            AppendTextToFile($"{DateTime.Now} {message}");
         }
 
         /// <summary>
         /// 指定した場所にあるテキストファイルに、指定したテキストを追加で書き込みます。
         /// ファイルが存在しない場合は新しく作成します。
         /// </summary>
-        /// <param name="filePath">テキストファイルのパス</param>
         /// <param name="textToAppend">追加するテキスト</param>
-        private static void AppendTextToFile(string filePath, string textToAppend)
+        private static void AppendTextToFile(string textToAppend)
         {
             try
             {
-                File.AppendAllText(filePath, textToAppend + Environment.NewLine);
-                Console.WriteLine($"テキストを '{filePath}' に追加しました。");
+                File.AppendAllText(LogFilePath, textToAppend + Environment.NewLine);
+                Console.WriteLine($"テキストを '{LogFilePath}' に追加しました。");
             }
             catch (Exception ex)
             {
